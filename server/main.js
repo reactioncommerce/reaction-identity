7451a1f1218816bf244e645650d8bd9e1e25a230
Eric Dobbertin <eric@dairystatedesigns.com>
2020-01-07 10:36:34 -0600

    fix: add missing onCreateUser
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- server/main.js
+++ server/main.js
@@ -1,7 +1,23 @@
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 import Logger from "@reactioncommerce/logger";
 import config from "./config.js";
 import { oauthLogin } from "./oauthMethods.js";
+
+/**
+ * @see: http://docs.meteor.com/#/full/accounts_oncreateuser
+ */
+Accounts.onCreateUser((options, user) => {
+  const roles = {};
+
+  // If there is not yet an "owner" user, make this one an owner
+  const ownerUser = Meteor.users.findOne({ "roles.__global_roles__": "owner" });
+  if (!ownerUser) roles.__global_roles__ = ["owner"]; // eslint-disable-line camelcase
+
+  user.roles = roles;
+
+  return user;
+});
 
 Meteor.methods({
   "getGraphQLApiUrl": () => config.API_URL,
